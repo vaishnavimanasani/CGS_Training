59f5e44233fe3faf0871225889b386fc1ba86b9d
vaishnavimanasani <138032668+vaishnavimanasani@users.noreply.github.com>
2024-09-05 14:32:55 +0530

    Add files via upload

---
+++ fibnocci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    let fib = [0, 1];
+    
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    
+    return fib;
+}
+
+let n = 100;
+let series = fibonacci(n);
+console.log(series);
